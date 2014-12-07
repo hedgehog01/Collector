9869edbaf1606e946c31499a406aa347aa7ab70a
nathanr <nathanr@NATHANR01-COMP.cellebrite.local>
2014-12-07 13:19:39 +0200

    Fixed add coin method and added message incase it fails.

--- src/com/jjlcollectors/controllers/AddCoinController.java
+++ src/com/jjlcollectors/controllers/AddCoinController.java
@@ -51,6 +51,7 @@ import javafx.util.StringConverter;
  */
 public final class AddCoinController implements Initializable
 {
+
     private static final Logger log = Logger.getLogger(AddCoinController.class.getName());
     private final String COLLECTION_TYPE_NOT_SELECTED = "Collection must be selected.\n";
     private final String CURRENCY_TYPE_NOT_SELECTED = "Currency must be selected.\n";
@@ -61,62 +62,59 @@ public final class AddCoinController implements Initializable
     private final String Mint_YEAR_EMPTY = "Enter Mint Year.\n";
     private final String Mint_YEAR_NONNUMERIC = "Mint Year must be numeric.\n";
     private final String COLLECTION_NAME_EXISTS = "Name already exists.\n";
-    
+    private final String COIN_NOT_ADDED = "Coin was not added.\nPlease try again later.";
 
     UUID userUUID = null;
     UUID collectionUUID = null;
     //public Coin (UUID userUUID,String name, CoinGrade grade, String facevalue, CoinCurrency currency, StringBuilder note, int coinYear, String coinMintMark, String buyPrice, String coinValue)
-    
+
     @FXML
     private TextField coinNameTxtField;
-    
+
     @FXML
     private TextField coinFaceValueTxtField;
-    
+
     @FXML
     private TextField coinNoteTxtField;
-    
+
     @FXML
     private TextField coinMintYearTxtField;
-    
+
     @FXML
     private TextField coinMintMarkTxtField;
-    
+
     @FXML
     private TextField coinBuyPriceTxtField;
-    
+
     @FXML
     private TextField coinValueTxtField;
-    
+
     @FXML
-    private ComboBox <CoinGrade> coinGradeComboBox;
-    
+    private ComboBox<CoinGrade> coinGradeComboBox;
+
     @FXML
     private Button addCoinBtn;
-    
+
     @FXML
     private Button cancelBtn;
-    
+
     @FXML
-    private ComboBox <CoinCurrency> currencyComboBox;
-    
+    private ComboBox<CoinCurrency> currencyComboBox;
+
     @FXML
-    private ComboBox <CollectionProperty> collectionComboBox;
-    
+    private ComboBox<CollectionProperty> collectionComboBox;
+
     @FXML
     private TextArea coinAddStatus;
-    
+
     @FXML
     private Label addCoinStatus;
-    
+
     @FXML
     private DatePicker coinBuyDatePicker;
-    
+
     private ObservableList<CollectionProperty> collectionComboListData = FXCollections.observableArrayList();
     private final ObservableList<String> currencyList = FXCollections.observableArrayList();
-    
-    
-
 
     /**
      * Initializes the controller class.
@@ -128,8 +126,8 @@ public final class AddCoinController implements Initializable
         currencyComboBox.getItems().addAll(CoinCurrency.values());
         coinGradeComboBox.getItems().addAll(CoinGrade.values());
         coinAddStatus.setVisible(false);
-        
-                // Define rendering of the list of values in ComboBox drop down. 
+
+        // Define rendering of the list of values in ComboBox drop down. 
         collectionComboBox.setCellFactory((comboBox) ->
         {
             return new ListCell<CollectionProperty>()
@@ -179,14 +177,13 @@ public final class AddCoinController implements Initializable
             log.log(Level.INFO, "Collection selction ComboBox Action, selected collection: {0}", selectedCollection.toString());
             collectionUUID = UUID.fromString(selectedCollection.getCollectionUUID());
         });
-    }    
-    
-    
+    }
+
     @FXML
     private void closeWindow()
     {
         Stage currentStage = (Stage) addCoinBtn.getScene().getWindow();
-            currentStage.close();
+        currentStage.close();
     }
 
     protected void setUserData(UUID userUUID, UUID collectionUUID)
@@ -194,34 +191,43 @@ public final class AddCoinController implements Initializable
         this.userUUID = userUUID;
         this.collectionUUID = collectionUUID;
     }
-    
+
     protected void setUserData(UUID userUUID)
     {
         this.userUUID = userUUID;
     }
-    
+
     @FXML
     private void addNewCoin()
     {
-        
+
         if (isCoinValid())
         {
+            coinAddStatus.setText("");
             coinAddStatus.setVisible(false);
             StringBuilder coinNote = new StringBuilder(coinNoteTxtField.getText());
             int coinYear = Integer.parseInt(coinMintYearTxtField.getText());
-            
+
             //UUID collectionUuid = DBCollectionConnect.getCollectionUUID (collectionComboBox.getValue().getCollectionUUID());
-            System.out.println ("collectionComboBox.getValue(): " + collectionComboBox.getValue());
-            System.out.println ("collection uuid is: " + collectionUUID);
+            System.out.println("collectionComboBox.getValue(): " + collectionComboBox.getValue());
+            System.out.println("collection uuid is: " + collectionUUID);
             //public Coin           (UUID userUUID,String name, CoinGrade grade, String facevalue, CoinCurrency currency, StringBuilder note, int coinYear, String coinMintMark, String buyPrice, String coinValue)
-            Coin newCoin = new Coin (userUUID,coinNameTxtField.getText(),coinGradeComboBox.getValue() ,coinFaceValueTxtField.getText(),currencyComboBox.getValue(),coinNote, coinYear,coinMintMarkTxtField.getText(),coinBuyPriceTxtField.getText(),coinValueTxtField.getText(),collectionUUID);
-            log.log(Level.INFO, "New Coin created, user uuid is {0}",userUUID.toString());
-            log.log(Level.INFO, "New Coin created, collection uuid is {0}",collectionUUID.toString());
-            DBCoinConnect.addCoin(newCoin);
+            Coin newCoin = new Coin(userUUID, coinNameTxtField.getText(), coinGradeComboBox.getValue(), coinFaceValueTxtField.getText(), currencyComboBox.getValue(), coinNote, coinYear, coinMintMarkTxtField.getText(), coinBuyPriceTxtField.getText(), coinValueTxtField.getText(), collectionUUID);
+            log.log(Level.INFO, "New Coin created, user uuid is {0}", userUUID.toString());
+            log.log(Level.INFO, "New Coin created, collection uuid is {0}", collectionUUID.toString());
+            if (DBCoinConnect.addCoin(newCoin))
+            {
+                log.log(Level.INFO, "coin added successfully");
+                closeWindow();
+            } else
+            {
+                log.log(Level.SEVERE, "coin NOT added successfully");
+                coinAddStatus.setText(COIN_NOT_ADDED);
+            }
         }
     }
-    
-        @FXML
+
+    @FXML
     private void loadUserData()
     {
         if (DBConnect.isDBConnectable())
@@ -230,7 +236,7 @@ public final class AddCoinController implements Initializable
             collectionComboListData.setAll(DBCollectionConnect.getUserCollections(userUUID));
             log.log(Level.INFO, "Adding data to collectionComboBox");
             collectionComboBox.getItems().setAll(collectionComboListData);
-            
+
         } else //connection not available
         {
             log.log(Level.WARNING, "Connection to DB unavailable. Can't get user collection");
@@ -241,103 +247,96 @@ public final class AddCoinController implements Initializable
     {
         boolean coinValid = true;
         StringBuilder issues = new StringBuilder("");
-        
+
         coinAddStatus.setVisible(!(coinValid));
-        
+
         //check name filed
         if (coinNameTxtField.getText() == null || coinNameTxtField.getText().trim().isEmpty())
         {
             issues.append(COLLECTION_NAME_EMPTY);
-             coinValid = false;
-             log.log(Level.INFO, "collection name is: {0}",coinNameTxtField.getText());
-        }
-        else if (DBCollectionConnect.getCollectionUUID(coinNameTxtField.getText()) != null) //check if collection name already exists
+            coinValid = false;
+            log.log(Level.INFO, "collection name is: {0}", coinNameTxtField.getText());
+        } else if (DBCollectionConnect.getCollectionUUID(coinNameTxtField.getText()) != null) //check if collection name already exists
         {
             issues.append(COLLECTION_NAME_EXISTS);
-             coinValid = false;
-             log.log(Level.INFO, "collection name already exists: {0}",coinNameTxtField.getText());
+            coinValid = false;
+            log.log(Level.INFO, "collection name already exists: {0}", coinNameTxtField.getText());
         }
-        
+
         //check currency combo box
         if (currencyComboBox.getValue() == null)
-         {
-             issues.append(CURRENCY_TYPE_NOT_SELECTED);
-             coinValid = false;
-             log.log(Level.INFO, "Currency selected is: {0}",currencyComboBox.getValue());
-         }
-        
+        {
+            issues.append(CURRENCY_TYPE_NOT_SELECTED);
+            coinValid = false;
+            log.log(Level.INFO, "Currency selected is: {0}", currencyComboBox.getValue());
+        }
+
         //check facevalue
-        
         if (coinFaceValueTxtField.getText() == null || coinFaceValueTxtField.getText().trim().isEmpty())
         {
             issues.append(FACEVALUE_EMPTY);
-             coinValid = false;
-             log.log(Level.INFO, "Face value is: {0}",coinFaceValueTxtField.getText());
-        }
-        
-        else if (!(isNumeric(coinFaceValueTxtField.getText())))
+            coinValid = false;
+            log.log(Level.INFO, "Face value is: {0}", coinFaceValueTxtField.getText());
+        } else if (!(isNumeric(coinFaceValueTxtField.getText())))
         {
-             issues.append(FACEVALUE_NONNUMERIC);
-             coinValid = false;
-             log.log(Level.INFO, "Face value is non-numeric: {0}",coinFaceValueTxtField.getText());
+            issues.append(FACEVALUE_NONNUMERIC);
+            coinValid = false;
+            log.log(Level.INFO, "Face value is non-numeric: {0}", coinFaceValueTxtField.getText());
         }
-        
+
         //check mint year
         if (coinMintYearTxtField.getText() == null || coinMintYearTxtField.getText().trim().isEmpty())
         {
             issues.append(Mint_YEAR_EMPTY);
-             coinValid = false;
-             log.log(Level.INFO, "Face value is: {0}",coinMintYearTxtField.getText());
-        }
-        
-        else if (!(isNumeric(coinMintYearTxtField.getText())))
+            coinValid = false;
+            log.log(Level.INFO, "Face value is: {0}", coinMintYearTxtField.getText());
+        } else if (!(isNumeric(coinMintYearTxtField.getText())))
         {
-             issues.append(Mint_YEAR_NONNUMERIC);
-             coinValid = false;
-             log.log(Level.INFO, "Face value is non-numeric: {0}",coinMintYearTxtField.getText());
+            issues.append(Mint_YEAR_NONNUMERIC);
+            coinValid = false;
+            log.log(Level.INFO, "Face value is non-numeric: {0}", coinMintYearTxtField.getText());
         }
-        
+
         //check collection combo box
         if (collectionComboBox.getValue() == null)
-         {
-             issues.append(COLLECTION_TYPE_NOT_SELECTED);
-             coinValid = false;
-             log.log(Level.INFO, "collection selected is: {0}",collectionComboBox.getValue());
-         }
-        
+        {
+            issues.append(COLLECTION_TYPE_NOT_SELECTED);
+            coinValid = false;
+            log.log(Level.INFO, "collection selected is: {0}", collectionComboBox.getValue());
+        }
+
         //check grade combo box
         if (coinGradeComboBox.getValue() == null)
-         {
-             issues.append(GRADE_NOT_SELECTED);
-             coinValid = false;
-             log.log(Level.INFO, "grade selected is: {0}",coinGradeComboBox.getValue());
-         }
+        {
+            issues.append(GRADE_NOT_SELECTED);
+            coinValid = false;
+            log.log(Level.INFO, "grade selected is: {0}", coinGradeComboBox.getValue());
+        }
         if (!(coinValid))
         {
             coinAddStatus.setVisible(!(coinValid));
             coinAddStatus.setBorder(Border.EMPTY);
             coinAddStatus.setText(issues.toString());
-            
+
         }
         return coinValid;
     }
-    
+
     /*
-    * method to check if string contains numeric value
-    */
+     * method to check if string contains numeric value
+     */
     private boolean isNumeric(String str)
     {
-        try  
-        {  
-            double d = Double.parseDouble(str);  
-        }  
-        catch(NumberFormatException nfe)  
-        {  
+        try
+        {
+            double d = Double.parseDouble(str);
+        } catch (NumberFormatException nfe)
+        {
             log.log(Level.INFO, "Non numeric value. Value was: {0}", str);
-            return false;  
-        }  
-        return true; 
-        
+            return false;
+        }
+        return true;
+
     }
-    
+
 }
